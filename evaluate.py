7f64a3a9447b5dc1c84b836b535f22e4f8b20b82
VINOTH KUMAR V <vk9360781721@gmail.com>
2025-11-26 19:59:37 +0530

    updated files

--- evaluate.py
+++ evaluate.py
@@ -1,4 +1,4 @@
-# we have to use similarity_search for manual retriving rather than as_retriever()
+# i have to use similarity_search for manual retriving rather than as_retriever()
 import time
 from langchain_chroma import Chroma
 from langchain_groq import ChatGroq
@@ -12,13 +12,37 @@ from torch import torch
 from bert_score import BERTScorer
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
-from langchain_openai import OpenAI
 from nltk.tokenize import regexp_tokenize
 import os
 import nltk
 import pandas as pd
 from dotenv import load_dotenv
+load_dotenv()
 #from langchain_classic import evaluation.scoring.eval_chain.ScoreStringEvalChain
+
+
+try:
+    nltk.data.find('taggers/averaged_perceptron_tagger_eng')
+except LookupError:
+    nltk.download('averaged_perceptron_tagger_eng', quiet=True)
+
+# Fallback for older NLTK versions
+try:
+    nltk.data.find('taggers/averaged_perceptron_tagger')
+except LookupError:
+    nltk.download('averaged_perceptron_tagger', quiet=True)
+
+try:
+    nltk.data.find('corpora/wordnet')
+except LookupError:
+    nltk.download('wordnet', quiet=True)
+    nltk.download('omw-1.4', quiet=True)
+
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords', quiet=True)
+
 
 model = HuggingFaceEmbeddings(model = 'all-MiniLM-L6-v2', model_kwargs = {'device': 'mps' if torch.mps.is_available() else 'cuda' if torch.cuda.is_available() else 'cpu'},
                               encode_kwargs = {'normalize_embeddings': False})
@@ -26,43 +50,56 @@ model = HuggingFaceEmbeddings(model = 'all-MiniLM-L6-v2', model_kwargs = {'devic
 llm = ChatGroq(model = 'llama-3.1-8b-instant', api_key = os.getenv('GROQ_API_KEY'), temperature=0.6, max_tokens=1000)
 vectorDB = Chroma(persist_directory='chromadb/',embedding_function=model)
 stop_words = set(stopwords.words('english')) 
-eval_model = OpenAI(model='gpt-40',temperature=0)
+eval_model = ChatGroq(model='openai/gpt-oss-20b',temperature=0,api_key=os.getenv('OPENAI_API_KEY'))
 
 def sementic_similarity(prediction, actual):
     scoring_model = BERTScorer(model_type='bert-base-uncased',lang='en', rescale_with_baseline=True)# rescale_with_baseline for scaling the values b default cosine similarites will be high if the 2 words belongs to some category but diff meaning, so we use rescale_with_baseline to avoid this
     # example: "dog" and "cat" are similar in terms of animals but different in meaning
     p,r, f1 = scoring_model.score(prediction, actual)
-    return p.mean().item(),r.mean().item(),f1.mean().item()
+    return p.tolist(),r.tolist(),f1.tolist(),p.mean().item(),r.mean().item(),f1.mean().item()
 
 def handling_sentence(preds):
         lemmatizer = WordNetLemmatizer()
         han_sen = preds.lower().strip()
-        han_sen = regexp_tokenize(han_sen,pattern=r'\w+')
+        tokens = regexp_tokenize(han_sen, pattern=r'\w+')
+        # remove stopwords
+        filtered_tokens = [t for t in tokens if t not in stop_words]
+        # POS tag expects a list of strings
+        tagged_tokens = nltk.pos_tag(filtered_tokens)
         predicted_chars = []
-        for char in han_sen:
-            if char not in stop_words:
-                tag = nltk.pos_tag(char)
-                predicted_chars.append(lemmatizer.lemmatize(char),tag)
+        for tok, tag in tagged_tokens:
+            predicted_chars.append((lemmatizer.lemmatize(tok), tag))
         return predicted_chars
 
 def exact_match(prediction, actual):    
-    exact_match = [] 
-    f1_score = []
-    def each_f1(prediction,actual):
-        if prediction == actual:
-            exact_match.append(1)
-        for i in range(len(prediction)):
-            pred_char = handling_sentence(prediction[i])
-            actual_char = handling_sentence(actual[i])
-            counts = 0
-            for char in pred_char:
-                if char in actual_char:
-                    counts += 1
-            precesion = counts /len(pred_char) if len(pred_char) >0 else 0
-            recall = counts / len(actual_char) if len(actual_char) >0 else 0
-            f1_score.append(2 *(precesion*recall)/(precesion+recall) if (precesion+recall) > 0 else 0)
-    each_f1(prediction, actual)
-    return exact_match,f1_score,sum(f1_score)/len(prediction), sum(exact_match)/len(prediction)
+    exact_match_list = [] # Use list for clarity
+    f1_score_list = []    # Use list for clarity
+    
+    # Iterate directly over the paired sentences
+    for i in range(len(prediction)):
+        
+        # 1. Exact Match Logic
+        if prediction[i] == actual[i]:
+            exact_match_list.append(1)
+        else:
+            exact_match_list.append(0)
+            
+        # 2. F1 Score Logic
+        pred_char = handling_sentence(prediction[i])
+        actual_char = handling_sentence(actual[i])
+        
+        counts = 0
+        for char in pred_char:
+            if char in actual_char:
+                counts += 1
+                
+        precesion = counts /len(pred_char) if len(pred_char) >0 else 0
+        recall = counts / len(actual_char) if len(actual_char) >0 else 0
+        f1 = 2 *(precesion*recall)/(precesion+recall) if (precesion+recall) > 0 else 0
+        f1_score_list.append(f1)
+        
+    # Return results
+    return exact_match_list, f1_score_list, sum(f1_score_list)/len(prediction), sum(exact_match_list)/len(prediction)
   
 
 
@@ -101,13 +138,14 @@ def retreival_qa_chain(inputs):
 
     results = []
     for i,questions in enumerate(inputs):
+        print(f"Processing question {i+1}/20")
         result = retrival_chain.invoke({'input': questions})
         results.append({
             "question": questions,
             "generated_answer": result["answer"],
             "retrieved_docs": result["context"] # This returns a list of Document objects
         })
-        print(f"Processed question {i+1}/{len(questions)}")
+        print(f"Processed question {i+1}")
     return results
 # result will be a list of dict with question, generated_answer, retrieved_docs
 
@@ -124,20 +162,21 @@ relevance_eval_prompt = {'relevance': (
 faithful_evaluator = load_evaluator(evaluator='labeled_criteria',llm=eval_model, criteria = faithful_eval_prompt, eval_prompt=faithful_eval_prompt) # labeled_criteria is used for finding faithfulness and .
 relevance_evaluator = load_evaluator(llm=eval_model,evaluator='criteria', criteria=relevance_eval_prompt) # criteria is used for finding relevance and Helpfulness.
 
-def halucination_evaluation(results):
+def halucination_evaluation(results,truths):
     metrics = {
         'faithfulness': [],
         'relevance': [],
         'latency': []
     }
-    for i,content in enumerate(results):
+    for content,truth in zip(results,truths):
         start_time = time.time()
         faithful_result = faithful_evaluator.evaluate_strings(input = content['question'],
                                                               prediction = content['generated_answer'],
-                                                              content = content['retrived_docs']) #result will be dict which contain {score: 0 or 1, value: Y or N,reasoning: content}
+                                                              reference = truth,
+                                                              content = content['retrieved_docs']) #result will be dict which contain {score: 0 or 1, value: Y or N,reasoning: content}
         relevance_result = relevance_evaluator.evaluate_strings(input = content['question'],
                                                                 prediction = content['generated_answer'],
-                                                                content = content['retrived_docs']) #result will be dict which contain {score: 0 or 1, value: Y or N,reasoning: content}
+                                                                content = content['retrieved_docs']) #result will be dict which contain {score: 0 or 1, value: Y or N,reasoning: content}
         end_time = time.time()
         latency = end_time - start_time
         metrics['latency'].append(latency)
@@ -158,15 +197,52 @@ def overlap_score(question,answer):
     return overlap_counts / len(answer_char) if len(answer_char) >0 else 0
 
 
-
 df = pd.read_csv('evaluate_data.csv')
 
 questions = df['question'].tolist()
 truth = df['answer'].tolist()
 answers = retreival_qa_chain(questions)
 
-results = retreival_qa_chain(questions)
-halucination_metrics = halucination_evaluation(results)
-sementic_precisions,sementic_recall,sementic_f1_scores = sementic_similarity(questions)
-exact_matches, f1_scores,avg_f1,avg_exactmatch = exact_match(prediction=[ pred for pred in answers['generated_answer']], actual=truth)
+halucination_metrics = halucination_evaluation(answers,truth)
+preds = [ pred['generated_answer'] for pred in answers]
+sementic_precisions,sementic_recall,sementic_f1_scores,mean_sementic_precisions,mean_sementic_recall,mean_sementic_f1_scores = sementic_similarity(prediction=preds,actual=truth)
+exact_matches, f1_scores,avg_f1,avg_exactmatch = exact_match(prediction=preds, actual=truth)
 latencys = halucination_metrics['latency']
+
+with open('evaluation_report.txt','w') as f:
+    print('---'*10+ ' ' + 'RAG Evaluation Results' + ' ' + '---' *10,file=f)
+    print(' ',file=f)
+    print('total questions evaluated: ', len(questions),file=f)
+    print(' ',file=f)
+    print('---'*10+ ' ' + 'Answers Evaluation Results' + ' ' + '---' *10,file=f)
+    print('Avg exact match scores is'+ ' '* 5 + ':' +' '+ str(avg_exactmatch),file=f)
+    print('Avg f1 scores is'+ ' '* 8 + ':' +' '+ str(avg_f1),file=f)
+    print('Avg sementic precision is'+ ' '* 5 + ':' +' '+ str(mean_sementic_precisions),file=f)
+    print('Avg sementic recall is'+ ' '* 7 + ':' +' '+ str(mean_sementic_recall),file=f)
+    print('Avg sementic f1 scores is'+ ' '* 6 + ':' +' '+ str(mean_sementic_f1_scores),file=f)
+
+
+    print(' ',file=f)
+    print('---'*10+ ' ' + 'Retrival Evaluation Results' + ' ' + '---' *10,file=f)
+    print('Avg faithfulness score is'+ ' '* 6 + ':' + ' ' + str(sum(halucination_metrics['faithfulness'])/len(questions)),file=f)
+    print('Avg relevance score is'+ ' '* 8 + ':' + ' ' + str(sum(halucination_metrics['relevance'])/len(questions)),file=f)
+
+    print(' ',file=f)
+    print('---'*10+ ' ' + 'Latency Evaluation Results' + ' ' + '---' *10,file=f)
+    print('Avg latency is'+ ' '* 13 + ':' + ' ' + str(sum(halucination_metrics['latency'])/len(questions)) + ' seconds',file=f)
+    print(' ',file=f)
+
+    print('---'*10+' '+'individual questions report:' +' '+'---'*10,file=f)
+    for question,answers,actutal_answer,match,f1_score,sementic_precision,sementic_recall,sementic_f1_score,relevance,latency in zip(questions,preds,truth,exact_matches,f1_scores,sementic_precisions,sementic_recall,sementic_f1_scores,halucination_metrics['relevance'],latencys):
+        print(f'question: {question},relevance score: {relevance}',file=f)
+        print(' ',file=f)
+        print(f'generated answer: {answers}',file=f)
+        print(' ',file=f)
+        print(f'actual answer: {actutal_answer}',file=f)
+        print(' ',file=f)
+        print(f'exact match score: {match}',file=f)
+        print(f'f1 score: {f1_score}',file=f)
+        print(f'sementic precision: {sementic_precision}',file=f)
+        print(f'sementic recall: {sementic_recall}',file=f)
+        print(f'sementic f1 score: {sementic_f1_score}',file=f)
+        print(f'Latency: {latency:.2f} seconds',file=f)
