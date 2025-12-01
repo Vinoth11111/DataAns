e518aa8f820060ce3dcef75e6e5460b5f71c38ce
VINOTH KUMAR V <vk9360781721@gmail.com>
2025-12-01 15:49:12 +0530

    updated start file which resolved grey screen issue

--- start.sh
+++ start.sh
@@ -8,4 +8,4 @@ echo "running the ingestion file"
 python3 ingest.py
 
 echo "starting the streamlit app"
-streamlit run app.py --server.port=7860 --server.address=0.0.0.0
+streamlit run app.py --server.port=7860 --server.address=0.0.0.0 --server.enableCORS=false --server.enableXsrfProtection=false
