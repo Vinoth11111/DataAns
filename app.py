0c5f79b0b2d019958c7d80708a499aab7aabdaab
VINOTH KUMAR V <vk9360781721@gmail.com>
2025-12-12 19:01:06 +0530

    removed commented blocks for deployment

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #for deployment
-"""__import__('pysqlite3')
+__import__('pysqlite3')
 import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')"""
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import streamlit as st
 from langchain_chroma import Chroma
